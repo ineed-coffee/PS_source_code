f42b2c5b8dad05c70ea074129359149fd056f708
ineedcoffee <leey93ssu@gmail.com>
2020-07-13 22:21:58 +0900

    1

--- Code_Forces/1375B-Neighbor_grid.py
+++ Code_Forces/1375B-Neighbor_grid.py
@@ -1,4 +1,28 @@
 from sys import *
+setrecursionlimit(10000)
+
+def match(x,y,cnt):
+    global flag
+    
+    for dx,dy in D:
+        nx,ny = x+dx , y+dy
+        if not ((0<=nx<n)and(0<=ny<m)):
+            continue
+        
+        if cnt and Grid[nx][ny]:
+            cnt-=1
+        elif cnt and not Grid[nx][ny]:
+            Grid[nx][ny]+=1
+            cnt-=1
+            match(nx,ny,Grid[nx][ny])
+        elif not cnt and Grid[nx][ny]:
+            Grid[x][y]+=1
+
+    if cnt:
+        flag=False
+    return
+
+
 
 input = stdin.readline
 
@@ -11,27 +35,21 @@ for case in range(T):
     for i in range(n):
         Grid.append([*map(int,input().split())])
 
+    Visited=[[False]*m for _ in range(n)]
+    flag=True
     for i in range(n):
-        for j in range(m):
-            cnt = Grid[i][j]
-            flag = True
-            if not cnt: continue
-            for di,dj in D:
-                ni,nj = i+di , j+dj
-                if not ((0<=ni<n)and(0<=nj<m)): continue
-                if cnt and Grid[ni][nj]: cnt-=1
-                elif cnt and not Grid[ni][nj]:
-                    Grid[ni][nj]+=1
-                    cnt-=1
-                elif not cnt and Grid[ni][nj]:
-                    flag = False
-                    break
-            if cnt: flag=False
-            if not flag: break
-        if not flag: break
-    if flag:
+        for j in range(n):
+            if Grid[i][j]:
+                match(i,j,Grid[i][j])
+            if not flag:
+                break
+        if not flag:
+            break
+
+    if not flag:
+        print('NO')
+    else:
         print('YES')
         for row in Grid:
             print(*row)
-    else:
-        print('NO')
+
