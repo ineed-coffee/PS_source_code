2a7b2d20cfdb3077817de55d61435a4e0731c90b
ineedcoffee <leey93ssu@gmail.com>
2021-03-14 20:56:12 +0900

    minor

--- auto_push_from_local.py
+++ auto_push_from_local.py
@@ -1,4 +1,3 @@
-import re
 import os
 import subprocess
 
@@ -16,36 +15,28 @@ if __name__ == "__main__":
     try:
         response= subprocess.check_output("git status -sb", universal_newlines=True)
     except subprocess.CalledProcessError:
-        print(1)
+        pass
 
     files = [path for path in response.split("\n")[1:]]
     py_file = [file[3:] for file in files if file.endswith(".py")]
     sql_file = [file[3:] for file in files if file.endswith(".sql")]
-    print(py_file)
-    print(sql_file)
 
     if py_file:
         try:
             for py in py_file:
                 py=reformat(py)
-                print(f"git add {py}")
                 response= subprocess.check_call(f"git add {py}")
-                print("added ps")
             response= subprocess.check_call('''git commit -m "daily ps :cloud:"''')
         except subprocess.CalledProcessError:
-            print(2)
+            print("Error while commiting .py files")
 
     if sql_file:
         try:
             for sql in sql_file:
                 sql=reformat(sql)
-                print(f"git add {sql}")
                 response= subprocess.check_call(f"git add {sql}")
-                print("added sql")
             response= subprocess.check_call('''git commit -m "daily sql :palm_tree:"''')
         except subprocess.CalledProcessError:
-            print(3)
+            print("Error while commiting .sql files")
 
     response= subprocess.check_call("git push")
-    print(response)
-
