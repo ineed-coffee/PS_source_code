2ed93b81c8420d30d6290e4a1ea03331b6274a71
ineedcoffee <leey93ssu@gmail.com>
2021-04-02 19:06:09 +0900

    daily ps :cloud:

---
+++ HackerRank/Interview Preparation Kit/Linked Lists/Insert a node at a specific position in a linked list.py
@@ -0,0 +1,62 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = SinglyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+
+
+        self.tail = node
+
+def print_singly_linked_list(node, sep, fptr):
+    while node:
+        fptr.write(str(node.data))
+
+        node = node.next
+
+        if node:
+            fptr.write(sep)
+
+# Complete the insertNodeAtPosition function below.
+
+#
+# For your reference:
+#
+# SinglyLinkedListNode:
+#     int data
+#     SinglyLinkedListNode next
+#
+#
+def insertNodeAtPosition(head, data, position):
+    if not head:
+        return head
+    
+    parent=head
+    for _ in range(position-1):
+        parent=parent.next
+        
+    child=parent.next
+    parent.next=SinglyLinkedListNode(data)
+    parent.next.next=child
+    return head
+
+if __name__ == '__main__':
